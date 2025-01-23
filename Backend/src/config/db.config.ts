b004eec1ca4cfe0c6e4ecb112fa1e7d05cd8b7a9
Bhavesh-Teli <telibhavesh11@gmail.com>
2025-01-23 12:39:46 +0530

    setup backend

---
+++ Backend/src/config/db.config.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+import { config } from './app.config';
+
+ const connectDatabase=async()=>{
+    try {
+        await mongoose.connect(config.MONGO_URI);
+        console.log("Mongo Database connected successfully")
+    } catch (error) {
+        console.log("Error connecting to Mongo database");
+        process.exit(1);
+    }
+    }
+    export default connectDatabase;
